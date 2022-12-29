b7eaf27c3c9194a50e6cf244291392a6119173ba
Jared Siemen <siemenjm@gmail.com>
2022-12-28 22:25:20 -0500

    Remove console logs. Export function

--- binary_search.js
+++ binary_search.js
@@ -18,9 +18,4 @@ function binarySearch(sortedArray, target) {
   return null;
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8));
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11));
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0));
-console.log(binarySearch([1, 3, 5, 7, 9], 4));
+module.exports = binarySearch;
