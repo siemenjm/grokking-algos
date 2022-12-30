e1f3db94c98b8687b44904fb373afc7965f5e189
Jared Siemen <siemenjm@gmail.com>
2022-12-30 11:18:55 -0500

    Initial commit

---
+++ quicksort.js
@@ -0,0 +1,9 @@
+// Quicksort
+// Another Divide and Conquer method
+//
+// 1. Pick a pivot
+// 2. Partition the array into two sub-arrays: elements less than pivot and elements greater than pivot
+// 3. Call quicksort recursively on two sub-arrays
+//
+// Base case: array of length 0 or 1
+// Recursive case: quicksort(lessArray) + pivot + quicksort(moreArray)
