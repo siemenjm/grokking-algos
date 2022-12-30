0d2c7e91290a4261ba50f54def2565a65e12b046
Jared Siemen <siemenjm@gmail.com>
2022-12-29 19:35:37 -0500

    Initial commit

---
+++ selection_sort.js
@@ -0,0 +1,5 @@
+// Selection sort
+// For example, find the largest value in an array
+// Loop through the array, tracking which index has the largest value
+// Store that value at the end of an (empty if first time through) array
+// Repeat until original array is empty
