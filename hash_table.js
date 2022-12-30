d9cc93e095517c5765c5eb731cc95a9f27ae7e3e
Jared Siemen <siemenjm@gmail.com>
2022-12-30 15:18:53 -0500

    Implement set, get, and remove methods

--- hash_table.js
+++ hash_table.js
@@ -12,8 +12,55 @@ class HashTable {
     this.table = new Array(maxSize);
     this.size = 0;
   }
+
+  // Hash method. Accepts key and turns it into an index
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i); // Sums the ASCI code of all the characters to make unique hash
+    }
+
+    return hash % this.table.length; // Makes sure the value returned is less than the max size/length of the hash table
+  }
+
+  // Insertion method
+  set(key, value) {
+    const index = this._hash(key); // Generates the index where the key:value pair will be placed
+    this.table[index] = [key, value]; // Stores the key:value pair in the hash table at the previously generated index
+    this.size++; // Increments size every time a value is inserted
+  }
+
+  // Search method
+  get(key) {
+    const index = this._hash(key); // Finds the index of the key in the hash table
+
+    return this.table[index]; // Returns the key:value pair at that index
+  }
+
+  // Delete method
+  remove(key) {
+    const index = this._hash(key);
+
+    if (this.table[index] && this.table[index].length) {
+      // Checking to see if the hash table has a truthy value and length greater than 0 at this index
+      this.table[index] = undefined;
+      this.size--; // Decrement the size property when key:value pair is removed
+      return true;
+    }
+
+    return false;
+  }
 }
 
 const prices = new HashTable(10);
 
-console.log(prices.size);
+prices.set('Apple', 5);
+prices.set('Banana', 1);
+prices.set('Pizza', 15);
+
+console.log(prices.get('Apple'));
+console.log(prices.get('Banana'));
+console.log(prices.get('Pizza'));
+
+prices.remove('Banana');
+console.log(prices.get('Banana'));
