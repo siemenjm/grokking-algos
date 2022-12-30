f6b7f591f5f7526bcad8db4e738878a3b2b5a300
Jared Siemen <siemenjm@gmail.com>
2022-12-30 14:54:16 -0500

    Initial commit

---
+++ hash_table.js
@@ -0,0 +1,7 @@
+// Hash Tables
+// Similar to object in JS, as in they are built of key:value pairs
+// Average Big O time of O(1) for search, insert, delete
+// Worst case Big O time of O(n) for search, insert, delete (if lots of collisions)
+//
+// Objects (and Maps) in JS are hash tables/maps that unfortunately overwrite data in collisions. You also don't know the size of the Object
+// Need to build own Hash table/map class with search, insert, and delete methods
