97eacac72caa0ddd2c31de26ceae15fb709d4116
Jared Siemen <siemenjm@gmail.com>
2022-12-31 10:37:32 -0500

    Initial commit

---
+++ linked_list.js
@@ -0,0 +1,6 @@
+// Linked List
+// Type of data structure that contains data and points to the next node in the list
+// Can easily add or remove node from the list without reorganizing the entire list, unlike arrays
+// Search operations are slow (no random access, have to start search from first node)
+// Uses more memory than array because of pointer storage
+// Singly linked (points to next), Doubly linked (points to previous and next), Circular linked (last node points to first node)
