25ddb22226d667a7193569bc07a50b7f09b4b0aa
Jared Siemen <siemenjm@gmail.com>
2022-12-31 10:32:45 -0500

    Initial commit

---
+++ graph.js
@@ -0,0 +1,9 @@
+// Graph
+// Made up of nodes/vertices and edges
+// Edges can be uni-directional or bi-directional (directed graph/undirected graph)
+//
+// Breadth-First-Search can answer:
+// * is there a path from node A to node B
+// * what is the shortest path from node A to node B
+//
+// Graph class consists of two data members: total number of nodes in the graph, and an array of linked lists to store adjacent nodes
