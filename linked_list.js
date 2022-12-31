48fd23813546027caa6bdcdefcac27ab5209459c
Jared Siemen <siemenjm@gmail.com>
2022-12-31 10:44:56 -0500

    Add size method

--- linked_list.js
+++ linked_list.js
@@ -4,6 +4,8 @@
 // Search operations are slow (no random access, have to start search from first node)
 // Uses more memory than array because of pointer storage
 // Singly linked (points to next), Doubly linked (points to previous and next), Circular linked (last node points to first node)
+//
+// Linked lists need four helper methods: size, clear, getLast, getFirst
 
 class ListNode {
   constructor(data) {
@@ -16,6 +18,18 @@ class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
+
+  // Returns the number of nodes present in the linked list
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+
+    return count;
+  }
 }
 
 let node1 = new ListNode(2); // Create new node which stores value of 2
@@ -24,3 +38,5 @@ node1.next = node2; // Points node1 to node2
 
 let list = new LinkedList(node1); // Create linked list with a head of node1
 console.log(list.head.next.data); // Access data stored in node2
+
+console.log('List size: ', list.size());
